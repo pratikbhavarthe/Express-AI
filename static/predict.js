a9186e66995c1ab4705be30baee762efdf7c3352
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2023-12-14 20:40:57 +0530

    Implemented image loading and model initialization

--- static/predict.js
+++ static/predict.js
@@ -11,3 +11,30 @@ let int1, int2, int3, int4, int5, int6;
 var array = [];
 var highestValue = 0;
 var highestIndex;
+
+//load uploaded files
+async function loadImage(event, id) {
+    var imageprev = document.getElementById(id + "prev");
+    var image = document.getElementById(id);
+    image.src = window.URL.createObjectURL(event.target.files[0]);
+    imageprev.src = window.URL.createObjectURL(event.target.files[0]);
+}
+
+//load the image model and setup the webcam
+async function init() {
+    if (refresh) {
+        refresh = false;
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+
+        //load the model and metadata
+
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+
+        
+    }
+}
+

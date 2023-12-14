e3410c03700f6b49fe5b86f20af6c9107bc4f76f
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2023-12-14 21:15:55 +0530

    Implemented webcam loop and prediction logic

--- static/predict.js
+++ static/predict.js
@@ -28,21 +28,20 @@ async function init() {
         const metadataURL = URL + "metadata.json";
 
         //load the model and metadata
-
         model = await tmImage.load(modelURL, metadataURL);
         maxPredictions = model.getTotalClasses();
 
         //ease function to setup a webcam
         const flip = true; //whether to flip the webcam
-        webcam = new tmImage.Webcam(400, 300, flip); //width/height/flip
+        webcam = new tmImage.Webcam(500, 300, flip); //width/height/flip
         await webcam.setup(); //request access to the webcam
         await webcam.play();
 
         //add elements to the DOM
     document.getElementById("webcam-container").appendChild(webcam.canvas);
-    document.getElementById("init").innerHTML = "in progress";
-    document.getElementById("init").style.background = " #fcf5e4";
-    document.getElementById("init").style.color = " #ffb800";
+    document.getElementById("init").innerHTML = "on going";
+    document.getElementById("init").style.background = " #1778fb";
+    document.getElementById("init").style.color = " #9fe870";
 
     document.getElementById("label").style.display = "block";
     document.getElementById("howto").style.display = "none";
@@ -83,3 +82,117 @@ async function init() {
   }
 }
 
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+  }
+  
+  async function toggle() {
+    console.log(document.getElementById("window").style.visibility);
+    if (document.getElementById("window").style.visibility === "visible") {
+      document.getElementById("window").style.visibility = "hidden";
+    } else {
+      document.getElementById("window").style.visibility = "visible";
+    }
+  
+    if (document.getElementById("closeBtn").style.transform === "rotate(0deg)") {
+      document.getElementById("closeBtn").style.transform = "rotate(180deg)";
+    } else {
+      document.getElementById("closeBtn").style.transform = "rotate(0deg)";
+    }
+  }
+  // run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+  
+    for (let i = 0; i < maxPredictions; i++) {
+      // console.log(maxPredictions);
+      array[i] = Number(prediction[i].probability.toFixed(2));
+      console.log(array);
+  
+      document.getElementsByClassName("labelItems")[i].style.display = "flex";
+  
+      highestValue = Math.max(...array);
+      highestIndex = array.indexOf(highestValue);
+  
+      console.log(highestIndex);
+  
+      if (highestIndex === 0) {
+        int1.style.opacity = 1;
+        int2.style.opacity = 0;
+        int3.style.opacity = 0;
+        int4.style.opacity = 0;
+        int5.style.opacity = 0;
+        int6.style.opacity = 0;
+      } else if (highestIndex === 1) {
+        int1.style.opacity = 0;
+        int2.style.opacity = 1;
+        int3.style.opacity = 0;
+        int4.style.opacity = 0;
+        int5.style.opacity = 0;
+        int6.style.opacity = 0;
+      } else if (highestIndex === 2) {
+        int1.style.opacity = 0;
+        int2.style.opacity = 0;
+        int3.style.opacity = 1;
+        int4.style.opacity = 0;
+        int5.style.opacity = 0;
+        int6.style.opacity = 0;
+      } else if (highestIndex === 3) {
+        int1.style.opacity = 0;
+        int2.style.opacity = 0;
+        int3.style.opacity = 0;
+        int4.style.opacity = 1;
+        int5.style.opacity = 0;
+        int6.style.opacity = 0;
+      } else if (highestIndex === 4) {
+        int1.style.opacity = 0;
+        int2.style.opacity = 0;
+        int3.style.opacity = 0;
+        int4.style.opacity = 0;
+        int5.style.opacity = 1;
+        int6.style.opacity = 0;
+      } else if (highestIndex === 5) {
+        int1.style.opacity = 0;
+        int2.style.opacity = 0;
+        int3.style.opacity = 0;
+        int4.style.opacity = 0;
+        int5.style.opacity = 0;
+        int6.style.opacity = 1;
+      }
+  
+      if (i === 0) {
+        c1n.innerHTML = prediction[i].className;
+        c1v.innerHTML = prediction[i].probability.toFixed(2);
+        c1p.value = prediction[i].probability.toFixed(2);
+      }
+      if (i === 1) {
+        c2n.innerHTML = prediction[i].className;
+        c2v.innerHTML = prediction[i].probability.toFixed(2);
+        c2p.value = prediction[i].probability.toFixed(2);
+      }
+      if (i === 2) {
+        c3n.innerHTML = prediction[i].className;
+        c3v.innerHTML = prediction[i].probability.toFixed(2);
+        c3p.value = prediction[i].probability.toFixed(2);
+      }
+      if (i === 3) {
+        c4n.innerHTML = prediction[i].className;
+        c4v.innerHTML = prediction[i].probability.toFixed(2);
+        c4p.value = prediction[i].probability.toFixed(2);
+      }
+      if (i === 4) {
+        c5n.innerHTML = prediction[i].className;
+        c5v.innerHTML = prediction[i].probability.toFixed(2);
+        c5p.value = prediction[i].probability.toFixed(2);
+      }
+      if (i === 5) {
+        c6n.innerHTML = prediction[i].className;
+        c6v.innerHTML = prediction[i].probability.toFixed(2);
+        c6p.value = prediction[i].probability.toFixed(2);
+      }
+    }
+  }
+  
